103403b9e8ab322999f11959ee581bb061a9c7af
Mohamadreza Mosalli <mohamadresaaa@gmail.com>
2024-09-22 22:42:44 +0330

    feat(docs): comment appModule and main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { SessionsModule } from './sessions/sessions.module';
 
+/**
+ * AppModule is the root module of the NestJS application.
+ * It imports various feature modules, defines controllers,
+ * and provides services required by the application.
+ */
 @Module({
   imports: [
+    // Common functionality shared across the application
     CommonModule,
+
+    // Database-related functionalities and connections
     DatabaseModule,
+
+    // Authentication features and services
     AuthModule,
+
+    // User management functionalities
     UsersModule,
+
+    // Session management functionalities
     SessionsModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
+// Import necessary modules from NestJS and compression middleware
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import compression from 'compression';
 
+/**
+ * Define an asynchronous function to bootstrap the application
+ */
 async function bootstrap() {
+  /**
+   * Create a new NestJS application instance using the root AppModule
+   */
   const app = await NestFactory.create(AppModule);
 
-  // Apply global compression middleware
+  /**
+   * Use compression middleware to enable gzip compression for responses
+   */
   app.use(
     compression({
-      level: 6, // Compression level (0-9)
-      filter: (req) => req.headers['accept-encoding']?.includes('gzip'), // Only compress certain content types or based on other criteria
-      threshold: 1024 // Compress responses larger than 1KB
+      // Set compression level (0-9), where 9 is the highest
+      level: 6,
+
+      // Only compress if the client supports gzip
+      filter: (req) => req.headers['accept-encoding']?.includes('gzip'),
+
+      // Only compress responses larger than 1 KB
+      threshold: 1024
     })
   );
 
+  /**
+   * Start listening for incoming requests on port 8080
+   */
   await app.listen(8080);
 }
+
+/**
+ * Invoke the bootstrap function to kickstart the application
+ */
 bootstrap();
